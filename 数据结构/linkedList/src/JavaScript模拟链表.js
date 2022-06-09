4a399a262cc48ffde7d97d302a32c3a9a824b2c7
ywanzhou <ywanzhou@foxmail.com>
2022-06-09 23:20:42 +0800

    js中的链表

---
+++ 数据结构/linkedList/src/JavaScript模拟链表.js
@@ -0,0 +1,10 @@
+// 随意定义一些元素
+const a = { value: 'a', next: null }
+const b = { value: 'b', next: null }
+const c = { value: 'c', next: null }
+const d = { value: 'd', next: null }
+
+// 实现链表
+a.next = b
+b.next = c
+c.next = d
