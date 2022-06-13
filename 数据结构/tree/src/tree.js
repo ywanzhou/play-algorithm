52b4cf868c998439c43ea309622e87ee081db81b
ywanzhou <ywanzhou@foxmail.com>
2022-06-13 23:49:38 +0800

    tree

---
+++ 数据结构/tree/src/tree.js
@@ -0,0 +1,23 @@
+const tree = {
+  value: 'A',
+  children: [
+    {
+      value: 'B',
+      children: [
+        { value: 'E', children: null },
+        { value: 'F', children: null },
+      ],
+    },
+    {
+      value: 'C',
+      children: [{ value: 'G', children: null }],
+    },
+    {
+      value: 'D',
+      children: [
+        { value: 'H', children: null },
+        { value: 'I', children: null },
+      ],
+    },
+  ],
+}
